eab3e13fad3cc204ff25d57fcbf9f5b46a5f60ce
frontendprof <https://github.com/frontendprof>
2021-04-25 14:17:38 +0500

    calculating get totals via reduce callback

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-import {CLEAR_CART,REM,INC,DEC} from "./actions";
+import {CLEAR_CART,REM,INC,DEC,GET_TOTALS} from "./actions";
 
 
 
@@ -20,12 +20,7 @@ function reducer(state,action){
     // }
 
     if(action.type===INC){
-      // let tempCart=state.cart.map(item=>{
-      //   if(item.id===action.payload.id){
-      //     item={...item,amount:item.amount+1}
-      //   }
-      //   return item;
-      // })
+
       return{
         ...state,cart:state.cart.map(item=>{
           if(item.id===action.payload.id){
@@ -57,12 +52,33 @@ function reducer(state,action){
     if(action.type===CLEAR_CART){
       return{...state, cart:[]}
     }
+
     if(action.type===REM){
       return{
         ...state,
         cart:state.cart.filter(item=>item.id!==action.payload.id)
       }
     }
+
+    if(action.type===GET_TOTALS){
+      let {total,amount}=state.cart.reduce((cartTotal,cartItem)=>{
+
+        const{amount,price}=cartItem;
+        const itemTotal=price * amount;
+
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+
+        return cartTotal;
+      },{
+        total:0,
+        amount:0
+      })
+      total=parseFloat(total.toFixed(2));
+      return{...state,total,amount}
+    }
+
+
     return state;
 }
   
